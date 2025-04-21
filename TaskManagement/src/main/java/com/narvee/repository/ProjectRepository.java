3ebd7ded38f2f2b4c59c40984ad988471c7082e2
NarveeTechnologies <158253867+NarveeTechnologies@users.noreply.github.com>
2025-04-21 13:26:07 +0530

    tms Security removed

--- TaskManagement/src/main/java/com/narvee/repository/ProjectRepository.java
+++ TaskManagement/src/main/java/com/narvee/repository/ProjectRepository.java
@@ -1,7 +1,5 @@
 package com.narvee.repository;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,10 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.narvee.dto.GetUsersDTO;
 import com.narvee.dto.ProjectDTO;
-
-import com.narvee.entity.TmsAssignedUsers;
 import com.narvee.entity.TmsProject;
 
 @Repository
